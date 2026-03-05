fe3fa5776c06b44f649ad1cc38d9465b62a457e9
Jia Le <jle26531@gmail.com>
2026-03-05 19:03:36 +0800

    add demo site / docs

---
+++ jiale-mr-demo-site/src/App.jsx
@@ -0,0 +1,135 @@
+import {ImagesList, RandomImage} from "jiale-mr";
+import {Fragment, useState} from "react";
+import pkg from "jiale-mr/package.json"
+
+function App() {
+
+    const [image, setImage] = useState(RandomImage());
+    const [tabs, setTabs] = useState("home");
+
+    return (
+        <>
+            <div className={"lg:flex min-h-screen w-full bg-black text-white"}>
+                <aside className={"flex-1 lg:block hidden"}>
+                </aside>
+                <main className={"flex-4 md:p-8 p-6"}>
+                    <h1 className={"text-3xl font-semibold font-mono"}>Jiale Mascot / Image Randomiser</h1>
+                    <p className={"text-gray-400"}>A client side random image library by Jia Le.</p>
+                    <div className={"flex gap-2 text-[16px] mt-4 items-center text-center"} id={"tabs"}>
+                        Navigation: &nbsp;
+                        <button
+                            onClick={() => {
+                                setTabs("home")
+                            }}
+                            className={`cursor-pointer ${tabs === "home" && "text-lime-300 underline font-mono font-semibold"}`}
+                        >
+                            Home
+                        </button>
+                        <button
+                            onClick={() => {
+                                setTabs("gallery")
+                            }}
+                            className={`cursor-pointer ${tabs === "gallery" && "text-lime-300 underline font-mono font-semibold"}`}
+                        >
+                            Gallery
+                        </button>
+                        <a
+                            href={"https://github.com/Jiale0709/jiale-mascot-randomiser"}
+                            target={"_blank"}
+                            className={`ml-auto flex  cursor-pointer text-blue-300  font-mono font-semibold`}
+                        >
+                            Github Repo
+                        </a>
+                    </div>
+                    {tabs === "home" && (
+                        <Fragment>
+                            <img
+                                className={"flex rounded-md md:max-w-2xl lg:max-w-3xl ml-auto mr-auto mt-6"}
+                                src={image.toString()}
+                                alt="Random Mascot"
+                            />
+                            <button
+                                className={"cursor-pointer mt-6 bg-lime-300 p-2 pl-4 pr-4 rounded-md text-black font-mono font-semibold text-sm"}
+                                onClick={() => {
+                                    setImage((prev) => {
+                                        let newImage = RandomImage();
+                                        while (newImage === prev) {
+                                            newImage = RandomImage();
+                                        }
+                                        return newImage;
+                                    });
+                                }}
+                            >
+                                Random Image
+                            </button>
+                        </Fragment>
+                    )}
+                    {tabs === "gallery" && (
+                        <Fragment>
+                            <h2 className={"mt-8 text-sm font-mono"}>
+                                These are the Image available in
+                                <code className={" ml-2 bg-gray-700 rounded-md pl-2 pr-2 text-sm pt-0.5 pb-0.5"}>
+                                    {pkg.version}
+                                </code>
+                                .
+                            </h2>
+                            <div className="grid grid-cols-1 sm:grid-cols-2 mt-4 gap-4 space-y-4">
+                                {ImagesList.map((i, key) => (
+                                    <div key={key}>
+                                        <img
+                                            onClick={() => {
+                                                window.open(`https://cdn.jsdelivr.net/gh/Jiale0709/jiale-mascot-randomiser@master/images/avif/${i}.avif`)
+                                            }}
+
+                                            src={`https://cdn.jsdelivr.net/gh/Jiale0709/jiale-mascot-randomiser@master/images/avif/${i}.avif`}
+                                            alt={"Demo Images " + (key + 1)}
+                                            loading={"lazy"}
+                                            className={"object-cover aspect-video rounded-md cursor-zoom-in"}
+                                        />
+                                        <h3 className={"mt-4 font-mono flex justify-between"}>
+                                            <span
+                                                className={"font-semibold font-mono"}>
+                                                {i.replaceAll("_", " ").slice(0, 1).toUpperCase() + i.replaceAll("_", " ").slice(1).toLowerCase()}
+                                            </span>
+                                            <div className={"flex gap-2 text-sm font-extrabold"}>
+                                                <button
+                                                    className={"cursor-pointer text-blue-400 hover:text-blue-500"}
+                                                    onClick={() => {
+                                                        navigator.clipboard.writeText(`https://cdn.jsdelivr.net/gh/Jiale0709/jiale-mascot-randomiser@master/images/avif/${i}.avif`)
+                                                    }}
+                                                >
+                                                    📋Copy URL
+                                                </button>
+                                                <button
+                                                    className={"cursor-pointer text-lime-400 hover:text-lime-500"}
+                                                    onClick={() => {
+                                                        fetch(`https://cdn.jsdelivr.net/gh/Jiale0709/jiale-mascot-randomiser@master/images/avif/${i}.avif`)
+                                                            .then(res => res.blob())
+                                                            .then(blob => {
+                                                                const blobUrl = URL.createObjectURL(blob);
+                                                                const a = document.createElement("a");
+                                                                a.href = blobUrl;
+                                                                a.download = `${i} - Jia Le Mascot x Image Randomiser - Jia Le's Project`;
+                                                                a.click();
+                                                                URL.revokeObjectURL(blobUrl);
+                                                            });
+                                                    }}
+                                                >
+                                                    👇Download
+                                                </button>
+                                            </div>
+                                        </h3>
+                                    </div>
+                                ))}
+                            </div>
+                        </Fragment>
+                    )}
+                </main>
+                <aside className={"flex-1 lg:block hidden"}>
+                </aside>
+            </div>
+        </>
+    )
+}
+
+export default App
