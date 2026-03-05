542ca6fb94ea15f5c786eb333c4a4b0d9df6a703
Jia Le <jle26531@gmail.com>
2026-03-05 23:35:46 +0800

    update docs

--- jiale-mr-demo-site/src/App.jsx
+++ jiale-mr-demo-site/src/App.jsx
@@ -16,12 +16,11 @@ function App() {
                     <h1 className={"text-3xl font-semibold font-mono"}>Jiale Mascot / Image Randomiser</h1>
                     <p className={"text-gray-400"}>A client side random image library by Jia Le.</p>
                     <div className={"flex gap-2 text-[16px] mt-4 items-center text-center"} id={"tabs"}>
-                        Navigation: &nbsp;
                         <button
                             onClick={() => {
                                 setTabs("home")
                             }}
-                            className={`cursor-pointer ${tabs === "home" && "text-lime-300 underline font-mono font-semibold"}`}
+                            className={`cursor-pointer ${tabs === "home" && "text-lime-300"}`}
                         >
                             Home
                         </button>
@@ -29,16 +28,24 @@ function App() {
                             onClick={() => {
                                 setTabs("gallery")
                             }}
-                            className={`cursor-pointer ${tabs === "gallery" && "text-lime-300 underline font-mono font-semibold"}`}
+                            className={`cursor-pointer ${tabs === "gallery" && "text-lime-300"}`}
                         >
                             Gallery
+                        </button>
+                        <button
+                            onClick={() => {
+                                setTabs("usage")
+                            }}
+                            className={`cursor-pointer ${tabs === "usage" && "text-lime-300"}`}
+                        >
+                            Usage
                         </button>
                         <a
                             href={"https://github.com/Jiale0709/jiale-mascot-randomiser"}
                             target={"_blank"}
                             className={`ml-auto flex  cursor-pointer text-blue-300  font-mono font-semibold`}
                         >
-                            Github Repo
+                            Repo
                         </a>
                     </div>
                     {tabs === "home" && (
@@ -83,7 +90,7 @@ function App() {
 
                                             src={`https://cdn.jsdelivr.net/gh/Jiale0709/jiale-mascot-randomiser@master/images/avif/${i}.avif`}
                                             alt={"Demo Images " + (key + 1)}
-                                            loading={"lazy"}
+                                            loading={key < 20 ? "eager" : "lazy"}
                                             className={"object-cover aspect-video rounded-md cursor-zoom-in"}
                                         />
                                         <h3 className={"mt-4 font-mono flex justify-between"}>
@@ -122,6 +129,47 @@ function App() {
                                     </div>
                                 ))}
                             </div>
+                        </Fragment>
+                    )}
+                    {tabs === "usage" && (
+                        <Fragment>
+                            <h2 className={"mt-8 mb-3 font-mono text-[16px]"}>Installation</h2>
+                            <pre className="bg-gray-700 text-gray-100 p-4 rounded-lg overflow-x-auto">
+                                <code>
+                                    npm install jiale-mr
+                                </code>
+                            </pre>
+                            <h2 className={"mt-8 mb-3 font-mono text-[16px]"}>Usage - RandomImage</h2>
+                            <pre className="bg-gray-700 text-gray-100 p-4 rounded-lg whitespace-pre-wrap wrap-break-word">
+                                <code className="font-mono text-sm">
+                                    {`import {RandomImage} from "jiale-mr";`}
+                                    <br/><br/>
+                                    // print a random image
+                                    <br/>
+                                    {`console.log(RandomImage());`}
+                                    <br/><br/>
+                                    //output: https://cdn.jsdelivr.net/gh/Jiale0709/jiale-mascot-randomiser@master/images/avif/rain.avif
+
+                                </code>
+                            </pre>
+                            <h2 className={"mt-8 mb-3 font-mono text-[16px]"}>Usage - ImagesList</h2>
+                            <pre className="bg-gray-700 text-gray-100 p-4 rounded-lg whitespace-pre-wrap wrap-break-word">
+                                <code className="font-mono text-sm">
+                                    {`import {ImagesList} from "jiale-mr";`}
+                                    <br/><br/>
+                                    // print an array of image name
+                                    <br/>
+                                    console.log(ImagesList);
+                                    <br/><br/>
+                                    /* output:
+                                    [
+                                        "beach",
+                                        "christmas_present",
+                                    ]
+                                    */
+                                </code>
+                            </pre>
+                            <br/>
                         </Fragment>
                     )}
                 </main>
